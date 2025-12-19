84fdc9e1eb930b98362b343a44a26eda05a19828
IKIII0 <rifkialsauqy@students.usu.ac.id>
2025-12-19 09:54:52 +0700

    yesi #3

--- caffinity-back-end/middleware/corsMiddleware.js
+++ caffinity-back-end/middleware/corsMiddleware.js
@@ -1,8 +1,13 @@
-const cors = require('cors');
+const cors = require("cors");
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
-  credentials: true
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://caffinity-front-end.vercel.app",
+    "https://caffinity-fe.vercel.app",
+  ],
+  credentials: true,
 };
 
 module.exports = corsOptions;
